bc1597b7867273c0c7b42a3a8da9740e2f8c81d5
840471259@qq.com <840471259@qq.com>
2017-07-03 12:23:01 +0800

    新建

--- leetcodelearn/src/SchoolFinderRealQuestion/CalculateMax.java
+++ leetcodelearn/src/SchoolFinderRealQuestion/CalculateMax.java
@@ -1,13 +1,37 @@
 package SchoolFinderRealQuestion;
+
+import com.sun.javafx.image.IntPixelGetter;
+
 /*
- * 风口的猪-中国的牛市
+ * 风口的猪-中国的牛市(分治法)
  */
 public class CalculateMax {
 	public static void main(String[] args) {
-		
+		int []prices={3,8,5,1,7,8};
+		System.out.println(calcuateMax(prices));
 	}
-	public static int calculateMax(int[] prices){
-		
-		return 0;
+	public static int getmax(int[]prices,int start,int end){
+		int max=0;
+		int min=prices[start];
+		for (int i = start+1; i<=end; i++) {
+			if (prices[i]-min>max) {
+				max=prices[i]-min;
+			}
+			if (prices[i]<min) {
+				min=prices[i];
+			}
+		}
+		return max;
 	}
+	public static int calcuateMax(int[] prices){
+		int sum=0;
+		for (int i = 0; i < prices.length; i++) {
+			int temp=getmax(prices, 0, i)+getmax(prices, i, prices.length-1);
+			if (temp>sum) {
+				sum=temp;
+			}
+		}
+		return sum;
+	}
+		
 }
